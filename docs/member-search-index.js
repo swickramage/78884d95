547e74107b48b693d08b31cc7c7747c8adbd1a52
Sadeep Wickramage <sadeep.wickramage@8x8.com>
2021-08-06 17:28:29 +0800

    Ok

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"GpgSignatureTest","l":"GpgSignatureTest()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"GpgSignatureTest","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ src/main/java/com/test/models/A.java
@@ -0,0 +1,7 @@
+public class A {
+    public A() {}
+
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+}
